d57718d6d7ef8a276484d77e6fdc3f02c1ee60ac
LINGYU\Lingyu <malingyu1991@gmail.com>
2017-06-22 22:12:36 -0500

    feat: add the function to calculate the mean image of all training data and subtract mean image in data-preprocessing

--- src/data/build_tfRecord.py
+++ src/data/build_tfRecord.py
@@ -10,8 +10,22 @@ from skimage import io
 import src.settings as settings
 
 
-def get_image_binary(filename):
-    return np.asarray(io.imread(filename), np.int32).tobytes()
+def get_image_binary(filename, offset):
+    image = np.asarray(io.imread(filename), np.int32)
+    if settings.ZERO_MEAN_IMAGE:
+        print('apply offset')
+        image -= offset
+    return image.tobytes()
+
+
+def get_mean():
+    total_mean = np.empty([settings.RAW_IMAGE_HEIGHT, settings.RAW_IMAGE_WIDTH, settings.RAW_IMAGE_DEPTH]).\
+        astype(np.int32)
+    total_mean[:, :, 0] = np.loadtxt(os.path.join(settings.IMG_PATH, 'R_mean.txt')).astype(np.int32)
+    total_mean[:, :, 1] = np.loadtxt(os.path.join(settings.IMG_PATH, 'G_mean.txt')).astype(np.int32)
+    total_mean[:, :, 2] = np.loadtxt(os.path.join(settings.IMG_PATH, 'B_mean.txt')).astype(np.int32)
+    total_mean[:, :, 3] = np.loadtxt(os.path.join(settings.IMG_PATH, 'I_mean.txt')).astype(np.int32)
+    return total_mean
 
 
 def write_to_tfrecord():
@@ -25,6 +39,7 @@ def write_to_tfrecord():
     df = df.set_index('image_name')
 
     total_size = len(img_list)
+    total_mean = get_mean()
 
     for idx, img in enumerate(img_list):
         img_name = img.split('\\')[-1].split('.')[0]
@@ -33,7 +48,7 @@ def write_to_tfrecord():
         for tag in tags:
             label[settings.LABELS[tag]] = 1
 
-        binary_image = get_image_binary(img)
+        binary_image = get_image_binary(img, total_mean)
         example = tf.train.Example(features=tf.train.Features(feature={
             'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label.tobytes()])),
             'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[binary_image]))
@@ -46,5 +61,26 @@ def write_to_tfrecord():
     writer_train.close()
     writer_validation.close()
 
+
+def calc_mean():
+    if os.path.exists(os.path.join(settings.IMG_PATH, 'R_mean.txt')) and \
+       os.path.exists(os.path.join(settings.IMG_PATH, 'G_mean.txt')) and \
+       os.path.exists(os.path.join(settings.IMG_PATH, 'B_mean.txt')) and \
+       os.path.exists(os.path.join(settings.IMG_PATH, 'I_mean.txt')):
+        return
+    img_list = glob.glob(os.path.join(settings.IMG_PATH, '*.tif'))
+    img_mean = np.zeros((settings.RAW_IMAGE_HEIGHT, settings.RAW_IMAGE_WIDTH, settings.RAW_IMAGE_DEPTH))
+    for img_path in img_list:
+        temp = np.asarray(io.imread(img_path), np.int32)
+        img_mean += temp
+    img_mean /= len(img_list)
+    img_mean = np.round(img_mean).astype(np.int32)
+    np.savetxt(os.path.join(settings.IMG_PATH, 'R_mean.txt'), img_mean[:, :, 0])
+    np.savetxt(os.path.join(settings.IMG_PATH, 'G_mean.txt'), img_mean[:, :, 1])
+    np.savetxt(os.path.join(settings.IMG_PATH, 'B_mean.txt'), img_mean[:, :, 2])
+    np.savetxt(os.path.join(settings.IMG_PATH, 'I_mean.txt'), img_mean[:, :, 3])
+
+
 if __name__ == "__main__":
+    calc_mean()
     write_to_tfrecord()
