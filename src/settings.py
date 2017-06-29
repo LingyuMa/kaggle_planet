cbacb57ec13a509727dc467fc85200ea97365a19
LINGYU\Lingyu <malingyu1991@gmail.com>
2017-06-29 18:10:50 -0500

    feat: update settings to adapt the new code structure, use network_id to switch different models

--- src/settings.py
+++ src/settings.py
@@ -8,52 +8,59 @@ TRAIN_DATA_PATH = os.path.join(CURRENT_PATH, r'../data/processed/train_set.txt')
 VALIDATION_DATA_PATH = os.path.join(CURRENT_PATH, r'../data/processed/validation_set.txt')
 
 # Labels lookup table
-LABELS = {'primary': 0,
-          'clear': 1,
-          'agriculture': 2,
-          'road': 3,
-          'water': 4,
-          'partly_cloudy': 5,
-          'cultivation': 6,
-          'habitation': 7,
-          'haze': 8,
-          'cloudy': 9,
-          'bare_ground': 10,
-          'selective_logging': 11,
-          'artisinal_mine': 12,
-          'blooming': 13,
-          'slash_burn': 14,
-          'blow_down': 15,
-          'conventional_mine': 16}
+WEATHER_LABELS = {'clear': 0,
+                  'partly_cloudy': 1,
+                  'haze': 2,
+                  'cloudy': 3}
 
-LABELS_SIZE = 17
+COMMON_LABELS = {'primary': 0,
+                 'agriculture': 1,
+                 'road': 2,
+                 'water': 3,
+                 'habitation': 4,
+                 'cultivation': 5,
+                 'bare_ground': 6}
+
+RARE_LABELS = {'selective_logging': 0,
+               'artisinal_mine': 1,
+               'blooming': 2,
+               'slash_burn': 3,
+               'blow_down': 4,
+               'conventional_mine': 5}
+
+AUGMENT_FLAG = {'partly_cloudy': 'flip_1',
+                'haze': 'rotate_45',
+                'cloudy': 'rotate_45'}  # to be added
+
+LABELS = [WEATHER_LABELS, COMMON_LABELS, RARE_LABELS]
+
+# Specify which network to train
+NETWORK_ID = 0
 
 # Train-validation separation parameters
 TRAIN_VALIDATION_RATIO = 0.8
 SEED = 448
-
-# Raw image shape
-RAW_IMAGE_WIDTH = 256
-RAW_IMAGE_HEIGHT = 256
-RAW_IMAGE_DEPTH = 4
 
 # Training parameters
 DATA_AUGMENTATION = False  # flip only
 NORMALIZE_IMAGE = False
 ZERO_MEAN_IMAGE = True
 
-IMAGE_WIDTH = 32
-IMAGE_HEIGHT = 32
+IMAGE_WIDTH = 64
+IMAGE_HEIGHT = 64
 IMAGE_DEPTH = 4
 
-NUM_RESIDUE_BLOCKS = 5
-WEIGHT_DECAY_FC = 0.0001
-WEIGHT_DECAY = 0.0001
+RAW_IMAGE_WIDTH = 256
+RAW_IMAGE_HEIGHT = 256
+RAW_IMAGE_DEPTH = 4
+
+WEIGHT_DECAY_FC = 0.0002
+WEIGHT_DECAY = 0.0002
 
 BATCH_SIZE = 128
 
 INITIAL_LEARNING_RATE = 0.01
-NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 33000
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 70750
 NUM_EPOCHS_PER_DECAY = 10
 LEARNING_RATE_DECAY_FACTOR = 0.1
 
@@ -61,7 +68,7 @@ MOVING_AVERAGE_DECAY = 0.9999
 
 # Log options
 LOG_PATH = os.path.join(CURRENT_PATH, r'../reports/planet_train')
-MAX_STEPS = 6000  # number of batches to run
+MAX_STEPS = 16000  # number of batches to run
 LOG_DEVICE_PLACEMENT = False
 LOG_FREQUENCY = 10
 
